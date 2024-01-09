a6a0f4d499b2c0f7e3823afc2b8ff3b92bb6a37b
drolinza <drolinza@gmail.com>
2024-01-09 22:03:49 +0800

    add error class in index.html, added css style for weather area and error class in main.css. added search area, error, if success, condition for update when user input city it will change the icon weather forecast, fitur error and hiding it if success fetch the data

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-const apiKey = '6ba9d4d0eb2654004a2e4a1ae8bd07ad';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=italy';
+const apiKey = "6ba9d4d0eb2654004a2e4a1ae8bd07ad";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=` + apiKey);
-    var data = await response.json();
+// search area
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+searchButton.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
 
-    console.log(data);
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=` + apiKey);
+  var data = await response.json();
+
+  // if error (user wrong input city name)
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+    // success
+  } else {
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    // hide information in the beginning
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+
+    // updare change icon everytime user search
+    const weatherIcon = document.querySelector(".weather-icon");
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "images/snow.png";
+    }
+  }
 }
-
-checkWeather();
